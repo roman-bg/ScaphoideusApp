9271b803e057353183265f2f2f31b1ad4d9de355
roman-bg <61371346+roman-bg@users.noreply.github.com>
2026-03-31 03:17:18 +0300

    Update sw.js

--- alergo/sw.js
+++ alergo/sw.js
@@ -1,1 +1,20 @@
+const CACHE_NAME = 'alergo-v1';
+// Описваме активите с пълния им път в папката
+const assets = [
+    '/alergo/',
+'/alergo/index.html',
+'/alergo/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
 
